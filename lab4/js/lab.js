c6171591de4603de11e25e6fc3e2b012bbd8c64e
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-01-25 18:54:35 -0800

    Update js and html

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+//Create timeline in frames-per-second.
+  //determine how many frames per second will be used and for how long.
+//Create joints on the rigged avatar.
+  //Create rigged joints or point selections.
+    //Research how to set certain pixels to be selectors.
+      //Set functions for move and roate tools.
+//Insert x, y,and z cooridantes to determine destination of joint.
+  //Once at destination, note again the cooridinates and save destination in timeline.
+    //Scrub or scroll through current animation
+      //If any inconsistencies, refer back to the x, y, and z coordinates.
+//Loop through animation.
+  //Beginning from first keyframe.
+    //Make loop indefinite.
