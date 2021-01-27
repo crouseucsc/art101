6e6ba57e38b2fef6bad9e6f8cdbb03e1de1fadaf
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-01-26 23:39:03 -0800

    Update js html and css

---
+++ lab5/js/lab5.js
@@ -0,0 +1,21 @@
+/*
+*Author: Christian Rouse <ctrouse@ucsc.edu>
+*Created: 26 January 2021
+*Licence: Public Domain
+*/
+
+//Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+//Calculate
+age = 2021 - year;
+
+//Output
+document.writeIn("Make: " + make + "<br>");
+document.writeIn("Model: " + model + "<br>");
+document.writeIn("Color: " + color + "<br>");
+document.writeIn("Year: " + year + "<br>");
+document.writeIn("Age: " + age + "<br>");
