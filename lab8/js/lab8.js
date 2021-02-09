0afe195d78a52a91b5cf574b4aac762761899ada
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-02-08 20:50:50 -0800

    packet

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -4,7 +4,6 @@ Professor: Wes Modes
 Class: Art-101-01: Introduction to Computer Programming for the Arts
 Due Date: 8 February 2021
 */
-function firstThing("Bibidi");
-function secondThing("Babidi");
-function thirdThing("Buu");
-console.log(firstThing);
+function thirdThing(test) {
+  console.log(test + ": This is the third thing. <br>")
+}
