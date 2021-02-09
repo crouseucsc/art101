b0ae676c2c82630d85f0e7532a03ff297d5cd44a
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-02-08 20:44:05 -0800

    lab 8 packet

---
+++ lab8/js/lab8.js
@@ -0,0 +1,10 @@
+/*
+Authored: Christian Rouse
+Professor: Wes Modes
+Class: Art-101-01: Introduction to Computer Programming for the Arts
+Due Date: 8 February 2021
+*/
+function firstThing("Bibidi");
+function secondThing("Babidi");
+function thirdThing("Buu");
+console.log(firstThing);
