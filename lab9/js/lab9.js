ac561489fc91cc3f808b4a9b13afe93f95b72acc
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-02-10 22:43:06 -0800

    Overhaul and lab9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,20 @@
+/*
+Authored: Christian Rouse
+Professor: Wes Modes
+Class: Art-101-01: Introduction to Computer Programming for the Arts
+Due Date: 11 February 2021
+*/
+var outputEl = document.getElementById("output"); /*Get element denoted with
+the id of output located in HTML.*/
+var new1El = document.createElement("h1"); /*Put HTML attribute inside the <div>
+tag/output element *not visible in HTML*.*/
+new1El.innerHTML = "If you see this and the font size doesn't make sense, then it's working as intended.";
+//Insert this string into the HTML attribute. *Is visible in HTML.*
+var new2El = document.createElement("h4"); /*Put HTML attribute inside the <div>
+tag/output element *not visible in HTML*.*/
+new2El.innerHTML = ".innerHTML really needs to be mentioned in the prompt."
+//Insert this string into the HTML attribute. *Is visible in HTML.*
+outputEl.appendChild(new1El); /*Force whatever is in new1El into the element
+denoted with the id of output in located HTML*/
+outputEl.appendChild(new2El); /*Force whatever is in new2El into the element
+denoted with the id of output in located HTML*/
