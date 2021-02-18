bffb0394c84a284d4b490d2690c6715334653bfe
crouseucsc <77128633+crouseucsc@users.noreply.github.com>
2021-02-18 00:16:38 -0800

    Massive Lab11 Update

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -3,15 +3,61 @@ Authored: Christian Rouse
 Professor: Wes Modes
 TA: Brock Stuessi
 Class: Art-101-01: Introduction to Computer Programming for the Arts
-Due Date: 16 February 2021
+Due Date: 18 February 2021
 */
-// var userName = window.prompt("Please enter your full name: ");
-buttonEl = document.getElementById('submit-button');//get Button element.
-buttonEl.addEventListener("click", function(){//Add event listener.
-  nameEl = document.getElementById('name');//get name element.
-  var userName = nameEl.value;//assign value of nameEl to userName.
-  userName = userName.toUpperCase().split('').sort().reverse().join('');//Mess with name like in lab7.
-  titleEl = document.getElementById('title');//get title element.
-  titleEl.innerText = userName;
-  console.log("user name:", userName);
+//Show Challenge Description
+var challengeSection = $("#challenge");
+challengeSection.append("<button id=challenge-button>Add Challenge Section</button>");
+$("#challenge-button").click(function(){
+  document.getElementById("challenge").innerHTML = "<p>Challenge Test</p>"
 });
+//Show Problem Description
+var problemSection = $("#problem");
+problemSection.append("<button id=problem-button>Add Problem Section</button>");
+$("#problem-button").click(function(){
+  document.getElementById("problem").innerHTML = "<p>Problem Test</p>"
+});
+//Show Results Desription
+var resultSection = $("#result");
+resultSection.append("<button id=result-button>Add Result Section</button>");
+$("#result-button").click(function(){
+  document.getElementById("result").innerHTML = "<p>Result Test</p>"
+});
+
+//JQuery Toggle Class Special
+$(document).ready(function(){
+  $("#css-button").click(function(){
+    $("#challenge").toggleClass("special");
+    $("#problem").toggleClass("special");
+    $("#result").toggleClass("special");
+  });
+});
+
+
+
+// //Challenge
+// challengeBut = document.getElementById("challenge-button");
+// challengeBut.addEventListener("click", function(){
+//   $("#challenge").innerHTML = "<p>Challenge Test</p>"
+// });
+//Problem
+// problemBut = document.getElementById("problem-button");
+// problemBut.addEventListener("click", function(){
+//   document.getElementById("problem").innerHTML = "<p>Problem Test</p>"
+// });
+// //Results
+// resultBut = document.getElementById("result-button");
+// resultBut.addEventListener("click", function(){
+//   document.getElementById("result").innerHTML = "<p>Result Test</p>"
+// });
+
+// var problemSection = $("#problem");
+// problemSection.append("<button id=problem-button>Add Problem Section</button>");
+// $("#problem-button").click("click", function(){
+//   document.getElementById("problem").innerHTML = "<h2>Problem</h2>"
+// })
+// var resultSection = $("#result");
+// resultSection.append("<button id=result-button>Add Result Section</button>");
+// $("#result-button").click("click", function(){
+//   document.getElementById("result").innerHTML = "<h2>Result</h2>"
+// })
